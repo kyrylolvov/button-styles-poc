f480fe40e838654f0f35dbf00404638bc2e36d90
kyrylolvov <kyrylolvov@icloud.com>
2024-02-14 23:49:28 -0500

    CHANGED: - Initiated project with Vite - Added shadcn

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="h-screen w-scree grid grid-cols-2">
+      <div className="bg-background">Nice</div>
+      <div className="bg-background">Nice</div>
+    </div>
+  );
+}
+
+export default App;
