c386e64077549e3814bf0a376b0edb269900863f
kyrylolvov <kyrylolvov@icloud.com>
2024-02-14 23:57:25 -0500

    CHANGED: - Updated App.tsx to include 4 variants of buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { Button } from './components/ui/button';
+
 function App() {
   return (
     <div className="h-screen w-scree grid grid-cols-2">
-      <div className="bg-background">Nice</div>
-      <div className="bg-background">Nice</div>
+      <div className="bg-background text-foreground grid place-items-center">
+        <div className="grid grid-cols-2 gap-4">
+          <Button>Solid Button</Button>
+          <Button variant="outline">Outline Button</Button>
+          <Button variant="secondary">Secondary Button</Button>
+          <Button variant="destructive">Destructive Button</Button>
+        </div>
+      </div>
+      <div className="dark bg-background text-foreground grid place-items-center">
+        <div className="grid grid-cols-2 gap-4">
+          <Button>Solid Button</Button>
+          <Button variant="outline">Outline Button</Button>
+          <Button variant="secondary">Secondary Button</Button>
+          <Button variant="destructive">Destructive Button</Button>
+        </div>
+      </div>
     </div>
   );
 }
